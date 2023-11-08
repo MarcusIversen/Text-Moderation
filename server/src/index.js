c13e91a6244b4024237cf58a378a47f2ef410a37
MarcusIversen <marcus.iversen@lego.com>
2023-11-08 12:07:45 +0100

    setup docker images

--- server/src/index.js
+++ server/src/index.js
@@ -26,6 +26,7 @@ const createModelEndpoint = (modelUrl) => async (req, res) => {
     }
 };
 
+
 app.post('/api/distilbert', createModelEndpoint('https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english'));
 app.post('/api/nsfw', createModelEndpoint('https://api-inference.huggingface.co/models/michellejieli/inappropriate_text_classifier'));
 app.post('/api/moderation', createModelEndpoint('https://api-inference.huggingface.co/models/KoalaAI/Text-Moderation'));

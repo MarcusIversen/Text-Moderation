714d4cbc61880b021bc6935cb8c97ba0aabb497c
MarcusIversen <marcus.iversen@lego.com>
2023-11-23 12:35:37 +0100

    added Ci and refactored db schema

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -4,6 +4,7 @@ import {UserService} from "../services/userService";
 import {UserController} from "../controllers/userController";
 import {ModerationController} from "../controllers/moderationController";
 import {ModerationService} from "../services/moderationService";
+import {modelEndpoint} from "../utils/utils";
 
 
 const router = express.Router();
@@ -26,6 +27,12 @@ router.delete('/users/delete/:userId', (req, res) => userController.deleteUser(r
 router.post('/moderation/moderate-text-input', (req, res) => moderationController.processTextInput(req, res));
 router.post('/moderation/bad-words-check', (req, res) => moderationController.badWordCheck(req, res));
 router.get('/moderation/list-of-bad-words', (req, res) => moderationController.getBadWordsList(req,res));
+
+//AI Routes
+router.post('/api/distilbert', modelEndpoint('https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english'));
+router.post('/api/nsfw', modelEndpoint('https://api-inference.huggingface.co/models/michellejieli/inappropriate_text_classifier'));
+router.post('/api/moderation', modelEndpoint('https://api-inference.huggingface.co/models/KoalaAI/Text-Moderation'));
+router.post('/api/contactInfo', modelEndpoint('https://api-inference.huggingface.co/models/jakariamd/opp_115_privacy_contact_information'));
 
 
 export default router;
